c9e87321c429e94fc5dda887aa65df5e39720373
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-21 01:02:18 -0300

    Preparando para injeção de dependência BaseRepository e BaseService

--- Api.Infra/Data/Repositories/BaseRepository.cs
+++ Api.Infra/Data/Repositories/BaseRepository.cs
@@ -8,34 +8,42 @@ namespace Api.Infra.Data.Repositories
 {
     public class BaseRepository<T> : IRepository<T> where T : BaseEntity
     {
-        private DataContext context = new DataContext();
+        private readonly DataContext _context;
+
+        public BaseRepository(DataContext context)
+        {
+            _context = context;
+        }
 
         public IList<T> SelectAll()
         {
-            return context.Set<T>().ToList();
+            return _context.Set<T>().ToList();
         }
 
         public T Select(int id)
         {
-            return context.Set<T>().Find(id);
+            return _context.Set<T>().Find(id);
         }
 
         public void Insert(T obj)
         {
-            context.Set<T>().Add(obj);
-            context.SaveChanges();
+            _context.Set<T>().Add(obj);
+
+            _context.SaveChanges();
         }
 
         public void Update(T obj)
         {
-            context.Entry(obj).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
-            context.SaveChanges();
+            _context.Entry(obj).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+
+            _context.SaveChanges();
         }
 
         public void Remove(int id)
         {
-            context.Set<T>().Remove(Select(id));
-            context.SaveChanges();
+            _context.Set<T>().Remove(Select(id));
+
+            _context.SaveChanges();
         }
 
     }
--- Api.Service/Services/BaseService.cs
+++ Api.Service/Services/BaseService.cs
@@ -9,11 +9,16 @@ namespace Api.Service.Services
 {
     public class BaseService<T> : IService<T> where T : BaseEntity
     {
-        private BaseRepository<T> repository = new BaseRepository<T>();
+        protected readonly IRepository<T> _repository;
+
+        public BaseService(IRepository<T> repository)
+        {
+            _repository = repository;
+        }
 
         public IList<T> Get()
         {
-            return repository.SelectAll();
+            return _repository.SelectAll();
         }
 
         public T Get(int id)
@@ -23,14 +28,15 @@ namespace Api.Service.Services
                 throw new ArgumentException("The id can't be zero.");
             }
 
-            return repository.Select(id);
+            return _repository.Select(id);
         }
 
         public T Post<V>(T obj) where V : AbstractValidator<T>
         {
             Validate(obj, Activator.CreateInstance<V>());
 
-            repository.Insert(obj);
+            _repository.Insert(obj);
+
             return obj;
         }
 
@@ -38,7 +44,8 @@ namespace Api.Service.Services
         {
             Validate(obj, Activator.CreateInstance<V>());
 
-            repository.Update(obj);
+            _repository.Update(obj);
+
             return obj;
         }
 
@@ -49,7 +56,7 @@ namespace Api.Service.Services
                 throw new ArgumentException("The id can't be zero.");
             }
 
-            repository.Remove(id);
+            _repository.Remove(id);
         }
 
         private void Validate(T obj, AbstractValidator<T> validator)
