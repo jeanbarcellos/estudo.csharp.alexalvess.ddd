260f629782fef4cacafd9c3d1ccb8e4f4f158308
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-24 22:31:22 -0300

    Domain - Ajustes em IService

--- Api.Domain/Interfaces/IService.cs
+++ Api.Domain/Interfaces/IService.cs
@@ -6,14 +6,14 @@ namespace Api.Domain.Interfaces
 {
     public interface IService<T> where T : BaseEntity
     {
+        IList<T> Get();
+
+        T Get(int id);
+
         T Post<V>(T obj) where V : AbstractValidator<T>;
 
         T Put<V>(T obj) where V : AbstractValidator<T>;
 
         void Delete(int id);
-
-        T Get(int id);
-
-        IList<T> Get();
     }
 }
